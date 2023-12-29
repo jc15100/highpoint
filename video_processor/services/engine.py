f53f91e6d23196e478d6ff9d906cbeb30997ff87
jcg <juanky.garcia15@gmail.com>
2023-12-29 15:10:12 -0500

    Remove test code

--- video_processor/services/engine.py
+++ video_processor/services/engine.py
@@ -11,20 +11,20 @@ class Engine:
         service = RacquetSportsMLService()
 
         # first check it's a supported sport video
-        supported = True#service.check_supported_sport(video_path)
+        supported = service.check_supported_sport(video_path)
 
         print("Supported sport? " + str(supported))
 
         if not supported:
             results = {'supported': False}
         else:
-            #results = service.run_processing(video_path, output_path)
-            results = {'supported': True, 'smashes': ['/media/smash-636.mp4', '/media/smash-723.mp4'], 'group_highlight': "/media//highlight-10.mp4", 
-                       'player_speeds':"{\"1\": [179.7213347412862, 179.72987851929983, 179.76330296116905, 179.85739903794337], " + 
-                       "\"2\": [173.7213347412862, 172.72987851929983, 18.76330296116905, 17.85739903794337], " + 
-                       "\"3\": [17.7213347412862, 1.72987851929983, 18.76330296116905, 145.85739903794337]}",
-                       "player_frames": ['/media/frame_0000.png','/media/frame_0001.png', '/media/frame_0002.png', '/media/frame_0003.png']
-                       }
+            results = service.run_processing(video_path, output_path)
+            # results = {'supported': True, 'smashes': ['/media/smash-636.mp4', '/media/smash-723.mp4'], 'group_highlight': "/media//highlight-10.mp4", 
+            #            'player_speeds':"{\"1\": [179.7213347412862, 179.72987851929983, 179.76330296116905, 179.85739903794337], " + 
+            #            "\"2\": [173.7213347412862, 172.72987851929983, 18.76330296116905, 17.85739903794337], " + 
+            #            "\"3\": [17.7213347412862, 1.72987851929983, 18.76330296116905, 145.85739903794337]}",
+            #            "player_frames": ['/media/frame_0000.png','/media/frame_0001.png', '/media/frame_0002.png', '/media/frame_0003.png']
+            #            }
         
         return results
         
