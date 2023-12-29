c15bc0ab767d0ba20382c7ee7bcf97f842ed2c15
jcg <juanky.garcia15@gmail.com>
2023-12-29 12:53:06 -0500

    Trying to remove matplotlib

--- ml/pipeline/segmentation/segmenter.py
+++ ml/pipeline/segmentation/segmenter.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-import logging
+#import matplotlib.pyplot as plt
+
 #from scipy.signal import argrelmin
 from PIL import Image
 
@@ -43,8 +43,8 @@ class MatchSegmenter:
         if flows is not None:
             print("Optical flow already computed for video, segmenting")
             if self.plotting == True:
-                plt.plot(flows[1])
-                plt.show()
+                #plt.plot(flows[1])
+                #plt.show()
         else:
             flows_per_player, player_frames = self.detection_flow(video, YOLOStep.person_name)
             # use player with id=1 for segmentation
@@ -163,8 +163,8 @@ class MatchSegmenter:
 
             if self.plotting == True:
                 # plot one of the speeds
-                plt.plot(speeds_per_player[1])
-                plt.show()
+                #plt.plot(speeds_per_player[1])
+                #plt.show()
 
             return (speeds_per_player, player_frames)
         else:
@@ -209,8 +209,8 @@ class MatchSegmenter:
         self.csv.saveArrayToCSV(flows_sums, "flows.csv")
         
         if self.plotting == True:
-            plt.plot(flows_sums, color = 'blue')
-            plt.show()
+            #plt.plot(flows_sums, color = 'blue')
+            #plt.show()
 
         return (frame_segments, flows_sums)
 
@@ -253,8 +253,8 @@ class MatchSegmenter:
         self.csv.saveArrayToCSV(flows_sums, self.filename)
 
         if self.plotting == True:
-            plt.plot(flows_sums, color = 'blue')
-            plt.show()
+            #plt.plot(flows_sums, color = 'blue')
+            #plt.show()
 
         return (frame_segments, flows_sums)
     
@@ -275,20 +275,20 @@ class MatchSegmenter:
 
     def _debug_plot_init(self, frame):
         if self.plotting == True:
-            plt.ion()
+            #plt.ion()
             hsv = np.zeros_like(frame)
             hsv[..., 1] = 255
             self.hsv = hsv
 
-            _, ax = plt.subplots(2,1)
-            self.ax = ax
+            #_, ax = plt.subplots(2,1)
+            #self.ax = ax
 
     def _debug_plot(self, axes, current_frame, flows_sums, flow, hsv):
         if self.plotting == True:
             axes[0].imshow(current_frame)
             axes[1].plot(flows_sums)
-            plt.draw()
-            plt.pause(0.001)
+            #plt.draw()
+            #plt.pause(0.001)
 
             # get vector magnitude and angle
             mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
