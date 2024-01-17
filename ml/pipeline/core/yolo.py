fef9fd8f4369226d398da2daa0adc9b59521acdd
jcg <juanky.garcia15@gmail.com>
2024-01-17 12:15:23 -0500

    Adding tests for YOLO and to create videos

--- ml/pipeline/core/yolo.py
+++ ml/pipeline/core/yolo.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 # YOLOV8, ultralytics has commercial license available. Can reconsider & use an equivalent opensource version.
 from ultralytics import YOLO
@@ -8,6 +9,7 @@ class YOLOStep:
     # Constants used to extract key elements from YOLO results
     ball_name = 'sports ball'
     person_name = 'person'
+    scale_factor = 0.3
 
     def __init__(self):
         self.model = YOLO('yolov8n.pt')
@@ -19,3 +21,42 @@ class YOLOStep:
     def track(self, frame):
         results = self.model.track(frame)
         return results
+    
+    def process_video(self, video):
+        count = 0
+        while True:
+            current_frame = video.read_frame()
+            if current_frame is not None:
+                frame_small = cv2.resize(current_frame, (0, 0), fx = self.scale_factor, fy = self.scale_factor)
+                results = self.track(frame_small)
+                annotated_frame = results[0].plot()
+                path = "frame-"+str(count)+".png"
+                cv2.imwrite(path, annotated_frame)
+                count+=1
+            else:
+                break
+        
+        frames_directory = '.'
+
+        # Get the list of JPEG files in the directory
+        frames = [f for f in os.listdir(frames_directory) if f.endswith('.png')]
+
+        # Sort the files to maintain the correct order
+        frames.sort()
+
+        # Get the first frame to obtain image dimensions
+        first_frame_path = os.path.join(frames_directory, frames[0])
+        first_frame = cv2.imread(first_frame_path)
+        height, width, layers = first_frame.shape
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        video_writer = cv2.VideoWriter("test.mp4", fourcc, 30.0, (width, height))  
+
+        # Iterate through each JPEG file and write it to the video file
+        for frame in frames:
+            frame_path = os.path.join(frames_directory, frame)
+            frame = cv2.imread(frame_path)
+            video_writer.write(frame)
+
+        # Release the VideoWriter object
+        video_writer.release()
--- ml/pipeline/tests.py
+++ ml/pipeline/tests.py
@@ -2,19 +2,22 @@ import unittest
 from segmentation.segmenter import MatchSegmenter
 from core.video import Video
 from core.yolo import YOLOStep
+import cv2
 
-class TestSegmenter(unittest.TestCase):
+class Tests(unittest.TestCase):
     testVideoPath = "/Users/juancarlosgarcia/Projects/video_tests/test-video-shortest.mp4"
+    testVideoPath2 = "/Users/juancarlosgarcia/Projects/video_tests/test_pickleball.mp4"
+    testVideoPath3 = "/Users/juancarlosgarcia/Projects/video_tests/test-tennis.mp4"
 
-    # def test_detection_flow(self):
-    #     test_video = Video(TestSegmenter.testVideoPath)
-    #     segmenter = MatchSegmenter(plotting=True)
+    def test_detection_flow(self):
+        test_video = Video(Tests.testVideoPath)
+        segmenter = MatchSegmenter(plotting=True)
 
-    #     flows = segmenter.detection_flow(test_video, YOLOStep.person_name)
-    #     self.assertTrue(len(flows) > 0)
+        flows = segmenter.detection_flow(test_video, YOLOStep.person_name)
+        self.assertTrue(len(flows) > 0)
     
     def test_segment(self):
-        test_video = Video(TestSegmenter.testVideoPath)
+        test_video = Video(Tests.testVideoPath)
         segmenter = MatchSegmenter(plotting=True)
 
         results_dict = segmenter.segment(video=test_video)
@@ -23,6 +26,16 @@ class TestSegmenter(unittest.TestCase):
         self.assertTrue(results_dict.__contains__('player_frames'))
         self.assertTrue(len(results_dict['player_speeds']) > 0)
         self.assertTrue(len(results_dict['player_frames']) > 0)
+
+    def test_yolo(self):
+        test_video = Video(Tests.testVideoPath)
+        test_video2 = Video(Tests.testVideoPath2)
+        test_video3 = Video(Tests.testVideoPath3)
+        yolo = YOLOStep()
+
+        #yolo.process_video(test_video)
+        #yolo.process_video(test_video2)
+        yolo.process_video(test_video3)
 
 if __name__ == '__main__':
     unittest.main()
